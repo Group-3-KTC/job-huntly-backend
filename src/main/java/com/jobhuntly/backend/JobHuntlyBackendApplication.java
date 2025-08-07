63294383b193a5793993d5ad8be4953bd084bb09
Nguyen Thanh Trong <ngthtrong0354@gmail.com>
2025-08-07 16:40:06 +0700

    first commit

---
+++ src/main/java/com/jobhuntly/backend/JobHuntlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobhuntly.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobHuntlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobHuntlyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobhuntly/backend/JobHuntlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobhuntly.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHuntlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
