5b2d0bc3882f753f5ca9d2c94059298208baf45d
Nguyen Thanh Trong <ngthtrong0354@gmail.com>
2025-08-11 10:04:45 +0700

    coding

--- src/main/java/com/jobhuntly/backend/entity/User.java
+++ src/main/java/com/jobhuntly/backend/entity/User.java
@@ -17,7 +17,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Integer id;
+    private Long id;
 
     @Column(nullable = false, unique = true, length = 255)
     private String email;
