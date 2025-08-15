2c62d14fe72aba13622418fbd85589b9942ab815
philau2512 <dplau25122002@gmail.com>
2025-08-15 15:51:30 +0700

    cors tạm thời

--- src/main/java/com/jobhuntly/backend/controller/JobController.java
+++ src/main/java/com/jobhuntly/backend/controller/JobController.java
@@ -11,6 +11,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${api.prefix}/job")
