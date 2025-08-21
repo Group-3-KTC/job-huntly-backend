b200ad0077279465a392b2e46b6edc285b4cecb7
HuyNguyen191103 <anhhuy598598@gmail.com>
2025-08-21 23:02:25 +0700

    update jwtProperties

--- src/main/java/com/jobhuntly/backend/security/jwt/JwtProperties.java
+++ src/main/java/com/jobhuntly/backend/security/jwt/JwtProperties.java
@@ -1,36 +1,21 @@
 package com.jobhuntly.backend.security.jwt;
 
 import com.jobhuntly.backend.config.AppProps;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 
+@Data
 @Component
-@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "security.jwt")
 public class JwtProperties {
-    private final AppProps props;
-
-    public String getSecret() {
-        return props.getJwt().getSecret();
-    }
-
-    public Duration getExpirySeconds() {
-        return props.getJwt().getExpirySeconds();
-    }
-
-    public String getIssuer() {
-        return props.getJwt().getIssuer();
-    }
-
-    public String getCookieName() {
-        return props.getJwt().getCookieName();
-    }
-
-    public String getRoleCookieName() {
-        return props.getJwt().getRoleCookieName();
-    }
+    private String secret;
+    private Duration expirySeconds;
+    private String issuer;
+    private String cookieName = "session";
+    private String roleCookieName = "role";
 }
 
