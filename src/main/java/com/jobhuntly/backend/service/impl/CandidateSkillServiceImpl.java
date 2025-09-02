0a822dec5857f595656d998294205c5902bf60af
Võ Hoàng Phúc (work) <phuc111239@gmail.com>
2025-09-02 11:51:37 +0700

    update candidate skill service

--- src/main/java/com/jobhuntly/backend/service/impl/CandidateSkillServiceImpl.java
+++ src/main/java/com/jobhuntly/backend/service/impl/CandidateSkillServiceImpl.java
@@ -135,16 +135,14 @@ public class CandidateSkillServiceImpl implements CandidateSkillService {
         Set<Category> categories = skill.getCategories();
         if (!categories.isEmpty()) {
             Category category = categories.iterator().next();
-            // Set child category (industry)
+
             response.setCategoryId(category.getId());
             response.setCategoryName(category.getName());
 
-            // Set parent category
             if (category.getParent() != null) {
                 response.setParentCategoryId(category.getParent().getId());
                 response.setParentCategoryName(category.getParent().getName());
             } else {
-                // If the category is a root category, set it as the parent
                 response.setParentCategoryId(category.getId());
                 response.setParentCategoryName(category.getName());
             }
